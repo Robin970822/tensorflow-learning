e04a59271803badf4a6df07e30d116fdff294e7d
Robin970822 <717740827@qq.com>
2018-01-19 21:06:38 +0800

    Add Pendulum using Prioritized DQN

--- Reinforcement/contents/Prioritized_Reply_DQN/run_Pendulum.py
+++ Reinforcement/contents/Prioritized_Reply_DQN/run_Pendulum.py
@@ -11,21 +11,22 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 
-env = gym.make('MountainCar-v0')
+env = gym.make('Pendulum-v0')
 env = env.unwrapped
 env.seed(21)
 MEMORY_SIZE = 10000
+ACTION_SPACE = 11
 
 sess = tf.Session()
 with tf.variable_scope('natural_DQN'):
     RL_natural = PrioritizedReplyDQN(
-        n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
+        n_actions=11, n_features=3, memory_size=MEMORY_SIZE,
         e_greedy_increment=0.00005, sess=sess, prioritized=False,
     )
 
 with tf.variable_scope('DQN_with_prioritized_replay'):
     RL_prio = PrioritizedReplyDQN(
-        n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
+        n_actions=11, n_features=3, memory_size=MEMORY_SIZE,
         e_greedy_increment=0.00005, sess=sess, prioritized=True, output_graph=True,
     )
 sess.run(tf.global_variables_initializer())
@@ -42,15 +43,26 @@ def train(RL):
             env.render()
 
             action = RL.choose_action(observation)
-
-            observation_, reward, done, info = env.step(action)
-
-            if done: reward = 10
+#
+#            observation_, reward, done, info = env.step(action)
+#
+#            if done: reward = 10
+        # convert to [-2,2] float actions
+            f_action = (action-(ACTION_SPACE-1)/2)/((ACTION_SPACE-1)/4)
+            observation_, reward, done, info = env.step(np.array([f_action]))
+            
+            # normalize to (-1, 0)
+            reward /= 10
 
             RL.store_transition(observation, action, reward, observation_)
 
             if total_steps > MEMORY_SIZE:
                 RL.learn()
+
+            if total_steps - MEMORY_SIZE > 20000:
+                steps.append(total_steps)
+                episodes.append(i_episode)
+                break
             
             if done:
                 steps.append(total_steps)
@@ -75,3 +87,4 @@ plt.ylabel('total training time')
 plt.xlabel('episode')
 plt.grid()
 plt.show()
+
