71ea0de2abe91b8f5a37543402257dc90c601f0c
Robin970822 <717740827@qq.com>
2018-01-19 18:23:43 +0800

    Prioritized Experience Reply DQN

---
+++ Reinforcement/contents/Prioritized_Reply_DQN/rl_brain.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 15:05:45 2018
+
+@author: hanxy
+"""
+
+import numpy as np
+import tensorflow as tf
+
+np.random.seed(1)
+tf.set_random_seed(1)
+
+class SumTree(object):
+    
+    data_pointer = 0
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        # [----parent nodes----][----leaves to recode priority----]
+        #   size: capacity - 1          size: capacity
+        self.tree = np.zeros(2 * capacity - 1)
+        # [----data frame----]
+        #   size: capacity
+        self.data = np.zeros(capacity, dtype=object)
+    
+    def add(self, p, data):
+        tree_idx = self.data_pointer + self.capacity - 1
+        self.data[self.data_pointer] = data     # update data frame
+        self.update(tree_idx, p)                # update tree frame
+        
+        self.data_pointer += 1
+        if self.data_pointer >= self.capacity:
+            self.data_pointer = 0
+    
+    def update(self, tree_idx, p):
+        change = p - self.tree[tree_idx]
+        self.tree[tree_idx] = p
+        # the propagate the change through tree
+        # update parent nodes
+        while tree_idx != 0:
+            tree_idx = (tree_idx - 1) // 2
+            self.tree[tree_idx] += change
+        
+    def get_leaf(self, v):
+        parent_idx = 0
+        while True:
+            cl_idx = 2 * parent_idx + 1
+            cr_idx = cl_idx + 1
+            if cl_idx >= len(self.tree):    # reach bottom
+                leaf_idx = parent_idx
+                break
+            else:
+                if v <= self.tree[cl_idx]:
+                    parent_idx = cl_idx
+                else:
+                    v -= self.tree[cl_idx]
+                    parent_idx = cr_idx    
+        data_idx = leaf_idx - self.capacity + 1
+        return leaf_idx, self.tree[leaf_idx], self.data[data_idx]
+    
+    @property
+    def total_p(self):
+        return self.tree[0]
+
+class Memeory(object):
+    
+    epsilon = 0.01      # small amount to avoid zero priority
+    alpha = 0.6         # [0,1] convert the important of TD error to priority
+    beta = 0.4          # importance-sampling from initial value increasing to 1
+    beta_increment_per_sampling = 0.001
+    abs_err_upper = 1.  # clipped abs error
+    
+    def __init__(self, capacity):
+        self.tree = SumTree(capacity)
+    
+    def store(self, transition):
+        max_p = np.max(self.tree.tree[-self.tree.capacity:])
+        if max_p == 0:
+            max_p = self.abs_err_upper
+        self.tree.add(max_p, transition)    # set the max p for new p
+    
+    def sample(self, n):
+        b_idx = np.empty((n,), dtype=np.int32)
+        b_memory = np.empty((n, self.tree.data[0].size))
+        ISWeights = np.empty((n, 1))
+        
+        pri_seg = self.tree.total_p / n     # priority segment
+        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])  # max = 1
+        
+        min_prob = np.min(self.tree.tree[-self.tree.capacity:]) / self.tree.total_p
+        for i in range(n):
+            a, b = pri_seg * i, pri_seg * (i + 1)
+            v = np.random.uniform(a, b)
+            idx, p, data = self.tree.get_leaf(v)
+            prob = p / self.tree.total_p
+            ISWeights[i, 0] = np.power(prob / min_prob, -self.beta)
+            b_idx[i], b_memory[i, :] = idx, data
+        return b_idx, b_memory, ISWeights
+    
+    def batch_update(self, tree_idx, abs_errors):
+        abs_errors += self.epsilon
+        clipped_errors = np.minimum(abs_errors, self.abs_err_upper)
+        ps = np.power(clipped_errors, self.alpha)
+        for ti, p in zip(tree_idx, ps):
+            self.tree.update(ti, p)
+    
+# Prioritized Reply Deep Q Network off-policy
+class PrioritizedReplyDQN:
+    def __init__(
+            self,
+            n_actions,
+            n_features,
+            learning_rate=0.01,
+            reward_decay=0.9,
+            e_greedy=0.9,
+            replace_target_iter=300,
+            memory_size=500,
+            batch_size=32,
+            e_greedy_increment=None,
+            prioritized=True,
+            output_graph=False,
+            sess=None
+    ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon_max = e_greedy
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.epsilon_increment = e_greedy_increment
+        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
+        
+        self.prioritized = prioritized
+        
+        # total learning step
+        self.learn_step_counter = 0
+
+        # consist of [target_net, evaluate_net]
+        self._build_net()
+        t_params = tf.get_collection('target_net_params')
+        e_params = tf.get_collection('eval_net_params')
+        self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
+        
+        if self.prioritized:
+            self.memory = Memeory(capacity=memory_size)
+        else:
+            self.memory = np.zeros((self.memory_size, n_features * 2 + 2))
+        
+        if sess is None:
+            self.sess = tf.Session()
+            self.sess.run(tf.global_variables_initializer())
+        else:
+            self.sess = sess
+        
+        if output_graph:
+            # $ tensorboard --logdir=logs
+            # tf.train.SummaryWriter soon be deprecated, use following
+            tf.summary.FileWriter("logs/", self.sess.graph)
+
+        self.cost_his = []
+
+    def _build_net(self):
+        # ------------------ build evaluate_net ------------------
+        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input
+        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')  # for calculating loss
+        if self.prioritized:
+            self.ISWeights = tf.placeholder(tf.float32, [None, 1], name='IS_Weights')
+        with tf.variable_scope('eval_net'):
+            # c_names(collections_names) are the collections to store variables
+            c_names, n_l1, w_initializer, b_initializer = \
+                ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES], 10, \
+                tf.random_normal_initializer(0., 0.3), tf.constant_initializer(0.1)  # config of layers
+
+            # first layer. collections is used later when assign to target net
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+                l1 = tf.nn.relu(tf.matmul(self.s, w1) + b1)
+
+            # second layer. collections is used later when assign to target net
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                self.q_eval = tf.matmul(l1, w2) + b2
+
+        with tf.variable_scope('loss'):
+            if self.prioritized:
+                self.abs_erros = tf.reduce_sum(tf.abs(self.q_target - self.q_eval), axis=1)
+                self.loss = tf.reduce_mean(self.ISWeights * tf.squared_difference(self.q_target, self.q_eval))
+            else:
+                self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
+        with tf.variable_scope('train'):
+            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+
+        # ------------------ build target_net ------------------
+        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')    # input
+        with tf.variable_scope('target_net'):
+            # c_names(collections_names) are the collections to store variables
+            c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+
+            # first layer. collections is used later when assign to target net
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+                l1 = tf.nn.relu(tf.matmul(self.s_, w1) + b1)
+
+            # second layer. collections is used later when assign to target net
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                self.q_next = tf.matmul(l1, w2) + b2
+
+    def store_transition(self, s, a, r, s_):
+        if self.prioritized:
+            transition = np.hstack((s, [a, r], s_))
+            self.memory.store(transition)
+        else:
+            if not hasattr(self, 'memory_counter'):
+                self.memory_counter = 0
+    
+            transition = np.hstack((s, [a, r], s_))
+    
+            # replace the old memory with new memory
+            index = self.memory_counter % self.memory_size
+            self.memory[index, :] = transition
+    
+            self.memory_counter += 1
+
+    def choose_action(self, observation):
+        # to have batch dimension when feed into tf placeholder
+        observation = observation[np.newaxis, :]
+
+        if np.random.uniform() < self.epsilon:
+            # forward feed the observation and get q value for every actions
+            actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
+            action = np.argmax(actions_value)
+        else:
+            action = np.random.randint(0, self.n_actions)
+        return action
+
+    def learn(self):
+        # check to replace target parameters
+        if self.learn_step_counter % self.replace_target_iter == 0:
+            self.sess.run(self.replace_target_op)
+            print('\ntarget_params_replaced\n')
+
+        
+        # sample batch memory from all memory
+        if self.prioritized:
+            tree_idx, batch_memory, ISWeights = self.memory.sample(self.batch_size)
+        else:
+            if self.memory_counter > self.memory_size:
+                sample_index = np.random.choice(self.memory_size, size=self.batch_size)
+            else:
+                sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
+            batch_memory = self.memory[sample_index, :]
+
+        q_next, q_eval = self.sess.run(
+            [self.q_next, self.q_eval],
+            feed_dict={
+                self.s_: batch_memory[:, -self.n_features:],  # fixed params
+                self.s: batch_memory[:, :self.n_features],  # newest params
+            })
+
+        # change q_target w.r.t q_eval's action
+        q_target = q_eval.copy()
+
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+        eval_act_index = batch_memory[:, self.n_features].astype(int)
+        reward = batch_memory[:, self.n_features + 1]
+
+        q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
+
+        # train eval network
+        if self.prioritized:
+            _, abs_errors, self.cost = self.sess.run([self._train_op, self.abs_erros, self.loss],
+                                                     feed_dict={
+                                                             self.s: batch_memory[:, :self.n_features],
+                                                             self.q_target: q_target,
+                                                             self.ISWeights: ISWeights})
+            self.memory.batch_update(tree_idx, abs_errors)
+        else:
+            _, self.cost = self.sess.run([self._train_op, self.loss],
+                                         feed_dict={self.s: batch_memory[:, :self.n_features],
+                                                    self.q_target: q_target})
+        self.cost_his.append(self.cost)
+
+        # increasing epsilon
+        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
+        self.learn_step_counter += 1
+
+    def plot_cost(self):
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
+        plt.ylabel('Cost')
+        plt.xlabel('training steps')
+        plt.show()
---
+++ Reinforcement/contents/Prioritized_Reply_DQN/run_MountainCar.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 15:05:56 2018
+
+@author: hanxy
+"""
+import gym
+from rl_brain import PrioritizedReplyDQN
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+env = gym.make('MountainCar-v0')
+env = env.unwrapped
+env.seed(21)
+MEMORY_SIZE = 10000
+
+sess = tf.Session()
+with tf.variable_scope('natural_DQN'):
+    RL_natural = PrioritizedReplyDQN(
+        n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
+        e_greedy_increment=0.00005, sess=sess, prioritized=False,
+    )
+
+with tf.variable_scope('DQN_with_prioritized_replay'):
+    RL_prio = PrioritizedReplyDQN(
+        n_actions=3, n_features=2, memory_size=MEMORY_SIZE,
+        e_greedy_increment=0.00005, sess=sess, prioritized=True, output_graph=True,
+    )
+sess.run(tf.global_variables_initializer())
+
+
+def train(RL):
+    total_steps = 0
+    steps = []
+    episodes = []
+    for i_episode in range(20):
+        print "Episode: %d" %(i_episode)
+        observation = env.reset()
+        while True:
+            env.render()
+
+            action = RL.choose_action(observation)
+
+            observation_, reward, done, info = env.step(action)
+
+            if done: reward = 10
+
+            RL.store_transition(observation, action, reward, observation_)
+
+            if total_steps > MEMORY_SIZE:
+                RL.learn()
+
+            if done:
+                steps.append(total_steps)
+                episodes.append(i_episode)
+                break
+
+            observation = observation_
+            total_steps += 1
+    return np.vstack((episodes, steps))
+
+print 'Natural DQN'
+his_natural = train(RL_natural)
+
+print 'Prioritized Reply DQN'
+his_prio = train(RL_prio)
+
+# compare based on first success
+plt.plot(his_natural[0, :], his_natural[1, :] - his_natural[1, 0], c='b', label='natural DQN')
+plt.plot(his_prio[0, :], his_prio[1, :] - his_prio[1, 0], c='r', label='DQN with prioritized replay')
+plt.legend(loc='best')
+plt.ylabel('total training time')
+plt.xlabel('episode')
+plt.grid()
+plt.show()
