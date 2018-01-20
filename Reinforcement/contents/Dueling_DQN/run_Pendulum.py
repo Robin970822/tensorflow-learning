c4c9aebd8f4ccb99d9709ed04965d8f1856f08dc
Robin970822 <717740827@qq.com>
2018-01-20 16:16:48 +0800

    Dueling DQN

---
+++ Reinforcement/contents/Dueling_DQN/run_Pendulum.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 15:05:56 2018
+
+@author: hanxy
+"""
+import gym
+from rl_brain import DuelingDQN
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+env = gym.make('Pendulum-v0')
+env = env.unwrapped
+env.seed(1)
+MEMORY_SIZE = 3000
+ACTION_SPACE = 25
+
+sess = tf.Session()
+with tf.variable_scope('Natural_DQN'):
+    natural_DQN = DuelingDQN(
+            n_actions=ACTION_SPACE,
+            n_features=3,
+            memory_size=MEMORY_SIZE,
+            e_greedy_increment=0.001,
+            dueling=False,
+            sess=sess
+            )
+with tf.variable_scope('Double_DQN'):
+    double_DQN = DuelingDQN(
+            n_actions=ACTION_SPACE,
+            n_features=3,
+            memory_size=MEMORY_SIZE,
+            e_greedy_increment=0.001,
+            dueling=True,
+            sess=sess,
+            output_graph=True)
+    
+sess.run(tf.global_variables_initializer())
+
+def train(RL):
+    acc_r = [0]
+    total_steps = 0
+    observation = env.reset()
+    while True:
+        env.render()
+        action = RL.choose_action(observation)
+        
+        # convert to [-2,2] float actions
+        f_action = (action-(ACTION_SPACE-1)/2)/((ACTION_SPACE-1)/4)
+        observation_, reward, done, info = env.step(np.array([f_action]))
+        
+        # normalize to (-1, 0)
+        reward /= 10
+        acc_r.append(reward + acc_r[-1])
+        
+        RL.store_transition(observation, action, reward, observation_)
+        
+        if total_steps > MEMORY_SIZE:
+            RL.learn()
+            
+        if total_steps - MEMORY_SIZE > 20000:
+            break
+        
+        observation = observation_
+        total_steps += 1
+    return RL.cost_his, acc_r
+
+print 'Natural DQN'
+c_natural, r_natural = train(natural_DQN)
+
+print 'Dueling DQN'
+c_dueling, r_dueling = train(double_DQN)
+
+
+plt.figure(1)
+plt.plot(np.array(c_natural), c='r', label='natural')
+plt.plot(np.array(c_dueling), c='b', label='dueling')
+plt.legend(loc='best')
+plt.ylabel('cost')
+plt.xlabel('training steps')
+plt.grid()
+
+plt.figure(2)
+plt.plot(np.array(r_natural), c='r', label='natural')
+plt.plot(np.array(r_dueling), c='b', label='dueling')
+plt.legend(loc='best')
+plt.ylabel('accumulated reward')
+plt.xlabel('training steps')
+plt.grid()
+
+plt.show()
+
+env.close()
+
+
