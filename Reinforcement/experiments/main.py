9bc6d381bde0c830be3d1c876ca71c0a5ce6bcfc
Robin970822 <717740827@qq.com>
2018-04-30 17:28:47 +0800

    rl for arm, build DDPG algorithm.

--- Reinforcement/experiments/main.py
+++ Reinforcement/experiments/main.py
@@ -6,6 +6,41 @@ from rl import DDPG
 # 设置全局变量
 MAX_EPISODES = 500
 MAX_EP_STEPS = 200
+ON_TRAIN = True
+
+
+def train():
+    # start training
+    for i in range(MAX_EPISODES):
+        s = env.reset()
+        ep_r = 0.
+        for j in range(MAX_EP_STEPS):
+            a = rl.choose_action(s)
+            s_, r, done = env.step(a)
+            rl.store_transition(s, a, r, s_)
+
+            ep_r += r
+            if rl.memory_full:
+                # start to learn once has fulfilled the memory
+                rl.learn()
+
+            s = s_
+            if done or j == MAX_EP_STEPS-1:
+                print 'Ep: %i | %s | ep_r: %.1f | step: %i' % (i, '---' if not done else 'done', ep_r, j)
+                break
+    rl.save()
+
+
+def eval():
+    rl.restore()
+    env.reset()
+    env.viewer.set_vsync(True)
+    s = env.reset()
+    while True:
+        env.render()
+        a = rl.choose_action(s)
+        print env.step(a)
+
 
 if __name__ == '__main__':
 
@@ -19,16 +54,8 @@ if __name__ == '__main__':
     rl = DDPG(a_dim, s_dim, a_bound)
 
     # 开始训练
-    for i in range(MAX_EPISODES):
-        s = env.reset()
-        for j in range(MAX_EP_STEPS):
-            env.render()
-            a = rl.choose_action(s)
-            s_, r, done = env.step(a)
+    if ON_TRAIN:
+        train()
+    else:
+        eval()
 
-            rl.store_transition(s, a, r, s_)
-
-            if rl.memory_full:
-                rl.learn()
-
-            s = s_
--- Reinforcement/experiments/rl.py
+++ Reinforcement/experiments/rl.py
@@ -1,12 +1,141 @@
+import tensorflow as tf
+import numpy as np
+
+# hyper parameters
+
+LR_A = 0.001  # learning rate for actor
+LR_C = 0.001  # learning rate for critic
+GAMMA = 0.9  # reward discount
+TAU = 0.01  # soft replacement
+MEMORY_CAPACITY = 30000
+BATCH_SIZE = 32
+
+
 class DDPG(object):
-    def __init__(self, a_dim, s_dim, a_bound,):
-        pass
+    def __init__(self, a_dim, s_dim, a_bound, ):
+        self.memory = np.zeros((MEMORY_CAPACITY, s_dim * 2 + a_dim + 1),
+                               dtype=np.float32)
+        self.pointer = 0
+        self.memory_full = False
+        self.sess = tf.Session()
+        self.a_replace_counter, self.c_replace_counter = 0, 0
+
+        self.a_dim, self.s_dim, self.a_bound = a_dim, s_dim, a_bound[1]
+
+        # placeholders
+        self.S = tf.placeholder(tf.float32, [None, s_dim], 's')
+        self.S_ = tf.placeholder(tf.float32, [None, s_dim], 's_')
+        self.R = tf.placeholder(tf.float32, [None, 1], 'r')
+
+        print "Building Graph..."
+        self._build_graph()
+        print "Graph Built"
 
     def choose_action(self, s):
-        pass
+        return self.sess.run(self.a, feed_dict={
+            self.S: s[None, :]
+        })[0]
 
     def learn(self):
-        pass
+        self.sess.run(self.soft_replace)
+
+        indices = np.random.choice(MEMORY_CAPACITY, size=BATCH_SIZE)
+
+        # self.memory : [s, a, reward, s_]
+        batch = self.memory[indices, :]
+        batch_s = batch[:, :self.s_dim]
+        batch_a = batch[:, self.s_dim:self.s_dim + self.a_dim]
+        batch_r = batch[:, -self.s_dim - 1:-self.s_dim]
+        batch_s_ = batch[:, -self.s_dim:]
+
+        self.sess.run(self.a_train, feed_dict={self.S: batch_s})
+        self.sess.run(self.c_train, feed_dict={self.S: batch_s,
+                                               self.a: batch_a,
+                                               self.R: batch_r,
+                                               self.S_: batch_s_})
 
     def store_transition(self, s, a, r, s_):
-        pass
+        transition = np.hstack((s, a, [r], s_))
+        index = self.pointer % MEMORY_CAPACITY
+        self.memory[index, :] = transition
+        self.pointer += 1
+        if self.pointer > MEMORY_CAPACITY:
+            self.memory_full = True
+
+    def _build_graph(self):
+        with tf.variable_scope('Actor'):
+            self.a = self.generator(self.S, scope='eval', trainable=True)
+            a_ = self.generator(self.S_, scope='target', trainable=False)
+        with tf.variable_scope('Critic'):
+            # assign self.a = a in memory when calculating q for td_error
+            # otherwise the self.a is from Actor when updating Actor
+            q = self.discriminator(self.S, self.a, scope='eval', trainable=True)
+            q_ = self.discriminator(self.S_, a_, scope='target', trainable=False)
+
+        # networks parameters
+        self.ae_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Actor/eval')
+        self.at_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Actor/target')
+        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Critic/eval')
+        self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+                                           scope='Critic/target')
+
+        # target net replacement
+        self.soft_replace = [[tf.assign(at, (1 - TAU) * at + TAU * ae),
+                              tf.assign(ct, (1 - TAU) * ct + TAU * ce)
+                              ]
+                             for at, ae, ct, ce in zip(self.at_params, self.ae_params, self.ct_params, self.ce_params)]
+
+        q_target = self.R + GAMMA * q_
+        # in the feed_dic for the td_error, the self.a should change to actions in memory
+        td_error = tf.losses.mean_squared_error(labels=q_target, predictions=q)
+        self.c_train = tf.train.AdamOptimizer(LR_C).minimize(td_error, var_list=self.ce_params)
+
+        a_loss = - tf.reduce_mean(q)  # maximize the q
+        self.a_train = tf.train.AdamOptimizer(LR_A).minimize(a_loss, var_list=self.ae_params)
+
+        self.sess.run(tf.global_variables_initializer())
+
+    def generator(self, s, scope, trainable):
+        '''
+        Generator
+
+        :param s:
+        :param scope:
+        :param trainable:
+        :return:
+        '''
+        with tf.variable_scope(scope):
+            l1 = tf.layers.dense(s, 300, activation=tf.nn.relu,
+                                 name='l1', trainable=trainable)
+            a = tf.layers.dense(l1, self.a_dim, activation=tf.nn.tanh,
+                                name='a', trainable=trainable)
+            return tf.multiply(a, self.a_bound, name='scaled_a')
+
+    def discriminator(self, s, a, scope, trainable):
+        '''
+        Discriminator
+
+        :param s:
+        :param a:
+        :param scope:
+        :param trainable:
+        :return:
+        '''
+        with tf.variable_scope(scope):
+            w1_s = tf.get_variable('w1_s', [self.s_dim, 300], trainable=trainable)
+            w1_a = tf.get_variable('w1_a', [self.a_dim, 300], trainable=trainable)
+            b1 = tf.get_variable('b1', [1, 300], trainable=trainable)
+            l1 = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1, name='l1')
+            l2 = tf.layers.dense(l1, 1, name='l2', trainable=trainable)
+            return l2
+
+    def save(self):
+        saver = tf.train.Saver()
+        saver.save(self.sess, './params', write_meta_graph=False)
+
+    def restore(self):
+        saver = tf.train.Saver()
+        saver.restore(self.sess, './params')
